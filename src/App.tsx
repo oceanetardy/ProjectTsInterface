756babbb920e3a82f69a880e7ae3b6f657e34c10
oceane tardy <tardy.oceane42@gmail.com>
2023-03-31 10:21:04 +0200

    [*] Test title et subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const App : FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Title title="essai de titre"  subtitle='test' />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -22,5 +23,17 @@ const App : FC = () => {
     </div>
   );
 }
+interface PropTitle {
+  title : string;
+  subtitle : string;
+}
 
+const Title :FC<PropTitle> = ({title, subtitle})=>{
+  return(
+      <>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      </>
+  )
+}
 export default App;
