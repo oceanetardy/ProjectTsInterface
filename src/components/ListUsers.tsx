b55e0743c5bf28f72c3b16fd55e2f92a50ccd092
oceane tardy <tardy.oceane42@gmail.com>
2023-03-31 16:20:09 +0200

    [*] Ajout list User

--- src/components/ListUsers.tsx
+++ src/components/ListUsers.tsx
@@ -1,11 +1,40 @@
-// import {FC, useState, useEffect} from "react";
-// import User from "../types/User";
-// import {getUsers} from "../services/user.service";
-//
-// const  ListUsers: FC = async (users:User[])=>{
-//     const [userList, setUsers] = useState<User[]>([]);
-//     useEffect( ()=>{
-//         const setUserList(getUsers())
-//     },[users])
-// }
-// export default ListUsers;
+import {FC, useState, useEffect, Dispatch} from "react";
+import User from "../types/User";
+import {getUsers} from "../services/user.service";
+
+const  ListUsers: FC = ()=>{
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect( ()=>{
+        const getData = async ()=>{
+            const response = await fetch('http://localhost:8080/users');
+            const users:User[] = await response.json();
+            console.log(users);
+                setUsers(users);
+        }
+        getData();
+    },[users]);
+
+    return (
+        <div>
+            <h3>Liste Users</h3>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                {users.map((val, key)=>{
+                    return (
+                        <tr key={key}>
+                            <td>{val.name}</td>
+                            <td>{val.email}</td>
+                        </tr>
+                    )
+                    }
+                )}
+            </table>
+        </div>
+
+    )
+}
+export default ListUsers;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header";
 import {FC} from "react";
 import Footer from "../components/Footer";
+import ListUsers from "../components/ListUsers";
 
 const Home:FC = ()=>{
     return(
         <div>
         <Header/>
-        <div>Contenu de la page</div>
+        <div> <ListUsers /></div>
             <Footer/>
         </div>
     )
