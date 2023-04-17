5cec9c052a23140f1831a774f6a43260ba0f3682
oceane tardy <tardy.oceane42@gmail.com>
2023-04-17 20:12:44 +0200

    :heavy_minus_sign: [-] Suppression code inutile

--- src/components/FormUser.tsx
+++ src/components/FormUser.tsx
@@ -29,50 +29,7 @@ const FormUser = ({ onSubmit }: FormProps) => {
         onSubmit(formData);
         setFormData({ name: "", email: "" });
     };
-    interface FormValues {
-        name: string;
-        email: string;
 
-
-    }
-
-    const initialValues: FormValues = {
-        name: '',
-        email: ''
-
-    };
-
-    const validationSchema = Yup.object({
-        name: Yup.string().min(3,"La longueur minimale n'est pas respectée").required('Le prénom est requis'),
-        email: Yup.string().email('Adresse email invalide').required('L\'adresse email est requise'),
-    });
-
-    const formik = useFormik({
-        initialValues,
-        validationSchema,
-        onSubmit
-    });
-    // return (
-    //     <form onSubmit={formik.handleSubmit}>
-    //         <div>
-    //             <label htmlFor="name">Name</label>
-    //             <input type="text" id="name" name="name" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} />
-    //             {formik.touched.name && formik.errors.name ? (
-    //                 <div className="error">{formik.errors.name}</div>
-    //             ) : null}
-    //         </div>
-    //         <div>
-    //             <label htmlFor="email">Email</label>
-    //             <input type="text" id="email" name="email" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
-    //             {formik.touched.email && formik.errors.email ? (
-    //                 <div className="error">{formik.errors.email}</div>
-    //             ) : null}
-    //         </div>
-    //         <button type="submit" disabled={!formik.isValid}>
-    //             Envoyer
-    //         </button>
-    //     </form>
-    // );
     return (
         <form onSubmit={handleSubmit}>
             <label>
