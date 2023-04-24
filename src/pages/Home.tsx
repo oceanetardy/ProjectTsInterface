00bdeb8bed6050e2b94c5b4469666064905e7f2c
oceane tardy <tardy.oceane42@gmail.com>
2023-04-24 22:46:15 +0200

    [*] Modif accueil

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,48 @@ import Title from "../components/Title";
 import Footer from "../components/Footer";
 import React, {FC} from "react";
 import '../assets/css/App.css';
+import {Col, Container} from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import ListTasksByUser from "../components/ListTaskByUser";
+import {Link} from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import {IoAddCircle, IoEyeSharp} from "react-icons/io5";
 
 
 const Home:FC = () =>{
     return (
 <header className="App-header">
-    <Title title="PROJET NFP 119"  subtitle='Application de gestion de tâches'  complement="Node JS / React / MongoDB"/>
 
 
+
+
+
+    <Container>
+        <Col md={12}>
+            <Row>
+                <Title title="PROJET NFP 119"  />
+            </Row>
+        </Col>
+
+
+        <Row>
+
+            <p> Application de gestion de tâche réalisée avec Node, React et Mongo DB.</p>
+
+        </Row>
+
+        <Row>
+            <p> Il est possible d'ajouter un utilisateur ainsi que de lui attribuer des tâches</p>
+        </Row>
+
+        <Row>
+
+            <p> Il est également possiblement de modifier la tâche.</p>
+        </Row>
+
+    </Container>
     <Footer/>
+
 
 </header>
     )}
